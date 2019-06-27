bc7741822ac19e0ae0fe4c456bdebd8b8167662d
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-27 09:02:02 +0200

    ft_check_newline function

--- ft_check_newline.c
+++ ft_check_newline.c
@@ -6,11 +6,11 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 07:53:37 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/27 08:59:10 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/27 09:01:54 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_check_newline(char **paragraph, char **row)
+int		ft_check_newline(char **paragraph, char **line)
 {
 	char	*temp_paragraph;
 	char	*check_paragraph;
@@ -25,7 +25,7 @@ int		ft_check_newline(char **paragraph, char **row)
 	}
 	temp_paragraph = &check_paragraph[i];
 	*temp_paragraph = '\0';
-	*row = ft_strdup(*paragraph);
+	*line = ft_strdup(*paragraph);
 	*paragraph = ft_strdup(temp_paragraph + 1);
 	return (1);
 }
