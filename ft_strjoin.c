58b46e259171aedfc0ef5c8aee4f30445f16e98e
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-27 07:52:22 +0200

    ft_strjoin function

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 08:58:51 by smsibi            #+#    #+#             */
+/*   Updated: 2019/06/27 07:51:28 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*fresh;
+	size_t	len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(fresh = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (*s1 != '\0')
+		fresh[i++] = *s1++;
+	while (*s2 != '\0')
+		fresh[i++] = *s2++;
+	fresh[i] = '\0';
+	return (fresh);
+}
